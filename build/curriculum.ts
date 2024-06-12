949a3ab5542416f8940ac52f34a9a878551175fa
Florian Dieminger <me@fiji-flo.de>
2024-06-12 22:34:00 +0200

    more landing fixes

--- build/curriculum.ts
+++ build/curriculum.ts
@@ -217,7 +217,15 @@ async function readCurriculumPage(
           summary,
           topic,
           slug,
-          children: children.length ? children : undefined,
+          children: children.length
+            ? children.map(({ url, title, summary, topic, slug }) => ({
+                url,
+                title,
+                summary,
+                topic,
+                slug,
+              }))
+            : undefined,
         }));
     } else if (attributes.template === Template.Overview) {
       modules = (await buildCurriculumIndex())
